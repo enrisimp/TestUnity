45a60a6b7b1598703f39242c04dd22e9490412fc
enrisimp <55219428+enrisimp@users.noreply.github.com>
2024-06-25 12:17:09 -0300

    primercommit

---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+    float xInput;
+    float zInput;
+
+    public float moveSpeed;
+
+    public AudioClip coinSound;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        float xVelocity = xInput * moveSpeed;
+        float zVelocity = zInput * moveSpeed;
+
+        rb.velocity = new Vector3(xVelocity, rb.velocity.y, zVelocity);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision);
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+
+            //GetComponent<AudioSource>().Play();
+            GetComponent<AudioSource>().PlayOneShot(coinSound);
+        }
+    }
+}
